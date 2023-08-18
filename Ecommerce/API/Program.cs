af63c2806fb9e6f15ed4131c7af56ba16dcf57d4
Winfred <winfrednginakilonzo@gmail.com>
2023-08-18 16:53:02 +0300

    Add documentation for API's

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,8 @@
 using DatabaseAccessLayer.BusinessLogic;
 using DatabaseAccessLayer.EcommerceDBContext;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,18 @@ builder.Services.AddSwaggerGen();
 //Registering DbContext
 builder.Services.AddDbContext<EcommerceContext>(options => options.UseSqlServer(builder.Configuration["DbConnection"]));
 var app = builder.Build();
+#region Run all Pending Migrations
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<EcommerceContext>();
+    var pendingMigrations = ctx.Database.GetPendingMigrations();
+    var migrator = ctx.GetInfrastructure().GetService<IMigrator>();
+    foreach (var migration in pendingMigrations)
+    {
+        migrator?.Migrate(migration);
+    }
+}
+#endregion
 app.MapGet("/", () => "Hello World!");
 //Register swagger API middle wares
 app.UseSwagger();
